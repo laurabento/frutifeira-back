c9151147778522b802393f48a5a52637edd5479a
Maria Gabriela <mgmakamine@gmail.com>
2022-03-19 16:57:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello"),
+    body: JSON.stringify("Hello dev"),
   };
   return response;
 };
