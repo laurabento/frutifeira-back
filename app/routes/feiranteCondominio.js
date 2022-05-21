9297ac80d80409fe140ed23410dc0d740287f3fb
Laura Bento <54412527+laurabento@users.noreply.github.com>
2022-05-20 21:27:09 -0300

    take off authorize

--- app/routes/feiranteCondominio.js
+++ app/routes/feiranteCondominio.js
@@ -126,7 +126,7 @@ router
 
 router
     .route("/condominio/:id")
-    .get(authorize(), async(req, res) => {
+    .get(async(req, res) => {
         const id = req.params.id;
         try {
             console.log(id);
